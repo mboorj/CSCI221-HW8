78c1c0923bd9f434fcd7ef88de7aadcfec0f9b6c
mboorj <94495260+mboorj@users.noreply.github.com>
2022-03-31 22:07:03 -0700

    Add files via upload

--- huffman.cc
+++ huffman.cc
@@ -27,18 +27,31 @@ Huffman::bits_t Huffman::encode(int symbol){
   return bool_bits;
 }
 
+int Huffman::decode(bool bit){
+  if (bit) {
+    auto poss = huff_tree_->get_child(Direction(1)); // go right if true
+  } else {
+    auto poss = huff_tree_->get_child(Direction(0)); // go left if false
+  }
+  if (poss->get_value() > 0) { // reached an actual symbol
+    return poss->get_value();
+  } else {
+    huff_tree_ = poss; // throw away the part of tree we won't use, bookmark poss for next check
+    return poss->get_value(); // will be -1
+  }
+}
 
 void Huffman::create_huff(){
-  auto temp_forest = std::make_shared<HForest>();
+  auto temp_forest = std::make_shared<HForest>(); // put 1 tree per character into a forest
   for (int i: freq_table_){
     temp_forest.add_tree(std::make_shared<HTree>(i, freq_table_[i]))
   }
 
-  while (temp_forest.size() > 1){
-    auto tree1 = temp_forest.pop_tree();
-    auto tree2 = temp_forest.pop_tree();
-    auto new_tree = std::make_shared<HTree>(-1, tree1.get_value()+tree2.get_value(), tree1, tree2);
-    temp_forest.add_tree(new_tree);
+  while (temp_forest.size() > 1){ // until everything is in one tree...
+    auto tree1 = temp_forest->pop_tree(); // remove and save lowest, second lowest
+    auto tree2 = temp_forest->pop_tree();
+    auto new_tree = std::make_shared<HTree>(-1, tree1->get_value()+tree2->get_value(), tree1, tree2); // merge as L,R in tree
+    temp_forest->add_tree(new_tree); // put new tree into forest, forest.size() -= 1
   }
-  huff_tree_ = temp_forest.pop_tree(); // janky as heck
+  huff_tree_ = temp_forest->pop_tree(); // get tree out of the forest, that's your huffman tree
 }
