9ff6a4e9ae39dab365ed97353ec29494496e5183
elijahw-s <101903226+elijahw-s@users.noreply.github.com>
2022-04-05 20:19:46 -0700

    more work

--- encoder.cc
+++ encoder.cc
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <filesystem>
-#include "bitio.hh"
+#include <vector>
 #include <fstream>
+#include "bitio.cc"
+#include "bitio.hh"
 
 
 main(int argc, char* argv[]){ // code copies from a tour of c++
@@ -12,7 +14,23 @@ main(int argc, char* argv[]){ // code copies from a tour of c++
     std::filesystem::path file_path {argv[1]}; 
 
     std::ifstream is (path, std::ifstream::binary);
-    
 
+    if (is) {
+        // get length of file:
+        is.seekg (0, is.end);
+        int length = is.tellg();
+        is.seekg (0, is.beg);
 
+        std::vector<char> char_vector;
+
+        for (int i; i < length; i++) {
+            char_vector.push_back(is.get());
+        }
+
+        is.close();
+
+        for (int i : char_vector) {
+            std::cout << char_vector[i];
+        }
+    }
 }
