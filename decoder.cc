0b2800ee09ca722cc23d49573ff14b6e0f14fac6
mboorj <94495260+mboorj@users.noreply.github.com>
2022-04-05 23:17:42 -0700

    Add files via upload

--- decoder.cc
+++ decoder.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <filesystem>
+#include <sstream>
 #include "bitio.hh"
 #include "huffman.hh"
 
@@ -8,5 +9,21 @@ int main(int argc, char* argv[]){ // code copies from a tour of c++
         std::cerr << "arguments expected\n";
         throw ;
     }
+
     std::filesystem::path p {argv[1]};
+    std::ifstream is (p, std::ifstream::binary);
+    p += ".plaintext"; // new file will be *.txt.comp.plaintext
+    std::ofstream os (p, std::ofstream::binary);
+
+    BitInput in = BitInput(is);
+    BitOutput out = BitOutput(os);
+    Huffman huff_tree = Huffman();
+
+    while (!is.eof()){ // if there's stuff in the file
+      auto sym = huff_tree.decode(in.input_bit()); // input_bit will automatically move to the next bit after grabbing
+      if (sym >= 0){ // if we've decoded a symbol, put it in the file
+        os.put(sym);
+      }
+    }
+    is.close();
 }
--- encoder.cc
+++ encoder.cc
@@ -2,6 +2,8 @@
 #include <filesystem>
 #include <vector>
 #include <fstream>
+#include <sstream>
+
 #include "bitio.hh"
 #include "huffman.hh"
 
@@ -14,25 +16,19 @@ int main(int argc, char* argv[]){ // code copies from a tour of c++
     std::filesystem::path file_path {argv[1]};
 
     std::ifstream is (file_path, std::ifstream::binary);
+    file_path+=".comp"; // new file will be *.txt.comp
+    std::ofstream os (file_path, std::ofstream::binary);
+    BitOutput out = BitOutput(os);
 
     std::vector<char> char_vector;
 
     Huffman huff_tree = Huffman();
 
-    if (is) {
-        // get length of file:
-        is.seekg (0, is.end);
-        int length = is.tellg();
-        is.seekg (0, is.beg);
-
-        for (int i = 0 ; i < length; i++) {
-            char_vector.push_back(is.get());
-        }
-
-        is.close();
+    while (!is.eof()){ // if there's still stuff in the istream
+      auto v = huff_tree.encode(is.get()); // get and encode the next char
+      for (auto bit : v){
+        out.output_bit(bit); // output the encoding bit by bit into the new file
+      }
     }
-
-    for(auto it=char_vector.begin(); it != char_vector.end(); it++) {
-        huff_tree.Huffman::encode(*it);
-    }
+    is.close();
 }
