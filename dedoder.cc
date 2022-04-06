8ee0a2671bb86a77ecff95e01d0794462cd6b11b
elijahw-s <101903226+elijahw-s@users.noreply.github.com>
2022-04-05 19:55:20 -0700

    work in progress

--- dedoder.cc
+++ dedoder.cc
@@ -1,3 +1,12 @@
-int main(){
-    
+#include <iostream>
+#include <filesystem>
+#include "bitio.cc"
+
+
+main(int argc, char* argv[]){ // code copies from a tour of c++
+    if (argc < 2) {
+        std::cerr << "arguments expected\n";
+        return 1;
+    }
+    std::filesystem::path p {argv[1]};
 }
--- encoder.cc
+++ encoder.cc
@@ -1,3 +1,18 @@
-int main(){
+#include <iostream>
+#include <filesystem>
+#include "bitio.cc"
+#include <fstream>
+
+
+main(int argc, char* argv[]){ // code copies from a tour of c++
+    if (argc < 2) {
+        std::cerr << "arguments expected\n";
+        return 1;
+    }
+    std::filesystem::path file_path {argv[1]}; 
+
+    std::ifstream is (path, std::ifstream::binary);
     
+
+
 }
